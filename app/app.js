a3236e0fc39d471d775b88df87e6dc21e81c63ed
davidalecrim <david.alecrim@cazoo.com>
2022-05-09 14:42:23 +0100

    feat(feature 1): add feature 1 to the application

--- app/app.js
+++ app/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/feature1', (req, res) => {
+  res.send('This is the implementation of feature 1!');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
